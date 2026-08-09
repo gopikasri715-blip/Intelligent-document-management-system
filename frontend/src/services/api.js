752f5563cd838e28a44be9723040e4f4b46df17f
gopikasri715-blip <gopikasri715@gmail.com>
2026-08-09 10:05:43 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://intelligent-document-management-system-2.onrender.com",
 });
 
 // Automatically attach JWT token to every request
