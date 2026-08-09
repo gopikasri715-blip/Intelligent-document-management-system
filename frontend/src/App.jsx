268b91095074effe8dc7a22fd67c92150e064f48
gopikasri715-blip <gopikasri715@gmail.com>
2026-08-09 10:14:36 +0530

    Fix login import case for Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
 import Upload from "./pages/Upload";
 import Search from "./pages/Search";
